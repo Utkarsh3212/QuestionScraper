6b194cf30696e7ff022010ac5dde889d66f943c6
Utkarsh3212 <127332769+Utkarsh3212@users.noreply.github.com>
2024-04-22 17:16:54 +0530

    removed useless files

--- ProblemFinder/10.py
+++
@@ -1,5 +0,0 @@
-text = "This is a sample string with various separators:sep1, sep2; sep3 - sep4"
-separators = [', ', '; ', ' - ',' ']
-
-result = filter(None, [text] + [part.strip() for sep in separators for part in text.split(sep)])
-print(list(result))
